48e52d06b8f95fbdfb00283e4b90a6cfad01e7be
Dmitry Smal <mialinx@yandex-team.ru>
2025-03-18 22:29:27 +0300

    new switchover algo

--- src/command_manager.py
+++ src/command_manager.py
@@ -6,6 +6,7 @@ _substitutions = {
     'primary_host': '%m',
     'timeout': '%t',
     'argument': '%a',
+    'wait': '%w',
 }
 
 
@@ -52,8 +53,8 @@ class CommandManager:
     def start_postgresql(self, timeout, pgdata):
         return self._exec_command('pg_start', timeout=timeout, pgdata=pgdata)
 
-    def stop_postgresql(self, timeout, pgdata):
-        return self._exec_command('pg_stop', timeout=timeout, pgdata=pgdata)
+    def stop_postgresql(self, timeout, pgdata, wait=True):
+        return self._exec_command('pg_stop', timeout=timeout, pgdata=pgdata, wait=('-w' if wait else '-W'))
 
     def get_postgresql_status(self, pgdata):
         return self._exec_command('pg_status', pgdata=pgdata)
