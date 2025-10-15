d617e7ee592dc71132a4a6420aebe14672e237a5
Dmitry Smal <mialinx@yandex-team.ru>
2025-10-15 19:22:52 +0300

    rewind old primary asap

--- src/main.py
+++ src/main.py
@@ -2086,7 +2086,8 @@ class pgconsul(object):
             if primary is not None:
                 # From here switchover can be considered successful regardless of this host state
                 self.zk.delete('%s/%s/op' % (self.zk.MEMBERS_PATH, helpers.get_hostname()))
-                self._return_to_cluster(primary, 'primary_after_switch', is_dead=True)
+                self._rewind_from_source(is_postgresql_dead=True, limit=timeout, new_primary=primary)
+                # we return true, as new primary found
                 return True
             logging.warning(f'SWITCHOVER_STATE_PATH ({self.zk.SWITCHOVER_STATE_PATH}) became None, but there is no one, who holds the leader lock.')
         else:
