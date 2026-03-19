466bc6b1be33dc1085b93d0803b08d1470365924
Andrey Kopylov <aakopylov@mail.ru>
2026-03-19 13:47:56 +0300

    MDB-41337: updated zookeeper to actual version (#129)
    Co-authored-by: Andrey Kopylov <kopylov74@yandex-team.ru>

--- docker/jepsen/save_logs.sh
+++ docker/jepsen/save_logs.sh
@@ -13,7 +13,6 @@ do
     docker exec pgconsul_postgresql${i}_1 cat \
         /var/log/postgresql/postgresql-$1-main.log > \
         logs/postgresql${i}/postgresql.log
-    docker exec pgconsul_zookeeper${i}_1 cat \
-        /var/log/zookeeper/zookeeper--server-pgconsul_zookeeper${i}_1.log > \
+    docker logs --tail all pgconsul_zookeeper${i}_1 > \
         logs/zookeeper${i}/zk.log 2>&1
 done
